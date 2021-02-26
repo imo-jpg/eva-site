b1dc7509b44280086cf2f167725ccc29ddd7e6d0
Imogen Todd <imo.todd@gmail.com>
2021-02-26 10:47:52 -0500

    adds various style changes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let links = document.getElementsByClassName("link");
+let linktext = document.getElementsByTagName("a");
+let body = document.getElementsByTagName("body")[0];
+let divs = document.getElementsByTagName("div");
+let svgs = document.getElementsByTagName("circle");
 
 for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("click", showPreview);
@@ -11,8 +15,7 @@ function showPreview(event) {
     
     let element = event.target.parentElement;
     let preview = element.nextSibling.nextSibling;
-    console.log(element.parentElement);
-    console.log(preview);
+    
 
     if (element.classList.contains("open") == false) {
         preview.classList.add("show");
@@ -23,5 +26,45 @@ function showPreview(event) {
         preview.classList.remove("show");
         element.classList.remove("open");
         element.classList.remove("expand");
+    }
+}
+
+function toSunRise() {
+    body.classList.add("sunrise");
+    body.classList.remove("sunset");
+
+    console.log(svgs[0].childNodes[1]);
+    
+    for (let i = 0; i < linktext.length; i++) {
+        linktext[i].classList.add("linksunrise");
+        linktext[i].classList.remove("linksunset");
+    }
+
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].classList.add("divsunrise");
+        divs[i].classList.remove("divsunset");
+    }
+
+    for (i in svgs) {
+        svgs[i].style.stroke="#5D1A38";
+    }
+}
+
+function toSunSet() {
+    body.classList.add("sunset");
+    body.classList.remove("sunrise");
+
+    for (let i = 0; i < linktext.length; i++) {
+        linktext[i].classList.add("linksunset");
+        linktext[i].classList.remove("linksunrise");
+    }
+
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].classList.add("divsunset");
+        divs[i].classList.remove("divsunrise");
+    }
+
+    for (i in svgs) {
+        svgs[i].style.stroke="#FFC105";
     }
 }
